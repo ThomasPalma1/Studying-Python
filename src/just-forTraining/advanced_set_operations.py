67b6007fbd567c4615c8b883f3826b179b296c7d
Thomas Palma <thomaspalma777@gmail.com>
2022-06-30 11:10:13 -0300

    adding contents to the repository
    - developing some simple code just to train;
    - improving gitignore for python projects;
    - performing the first folder structure.

---
+++ src/just-forTraining/advanced_set_operations.py
@@ -0,0 +1,5 @@
+friends = {"Bob", "Rolf", "Anne"}
+abroad = {"Bob", "Anne"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
---
+++ src/just-forTraining/intersection.py
@@ -0,0 +1,5 @@
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science = {"Bob", "Jen", "Adam", "Anne"}
+
+both = art.intersection(science)
+print (both)
