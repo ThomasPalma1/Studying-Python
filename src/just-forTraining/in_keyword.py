a8077b7eacd0ccdecb4a5a10786c25bbe163e199
Thomas Palma <thomaspalma777@gmail.com>
2022-07-03 15:18:11 -0300

    learning about: "in" keyword in python
    - learning about its definition and how to use it.

---
+++ src/just-forTraining/in_keyword.py
@@ -0,0 +1,9 @@
+movies_watched = ["The Matrix", "Green Book", "Her"]
+user_movie = input("Enter something you've watched recently: ")
+
+print(user_movie in movies_watched)
+
+# definition and usage:
+# └── the in keyword has two purposes:
+#     ├── is used to check if a value is present in a sequence (list, range, string etc.);
+#     └── is also used to iterate through a sequence in a for loop.
