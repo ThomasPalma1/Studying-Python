78c08d44c3b748fbd749c1635dfa5607a2797c65
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-07-05 11:34:48 -0300

    making a simple game of hitting the correct number
    - game focus is to use more than one structure if and improve code typing in python.

---
+++ src/just-forTraining/another_if_with-in.py
@@ -0,0 +1,17 @@
+number = 7
+user_input = input("Enter 'y' uf you would like to play: ").lower()
+
+if user_input == "y":
+    user_number = int(input("Guess our number: "))
+    if user_number == number:
+        print("You guessed correctly!")
+    elif abs(number - user_number) == 1:
+        print("You were off by one.")
+    else:
+        print("Sorry, it's wrong!")
+
+# if structure:
+# └── if
+#    ├── if
+#    ├── elif
+#    └── else
