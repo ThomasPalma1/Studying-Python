135f626fec7463466fd8364ca9ed35a82340c7e9
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-07-13 10:50:46 -0300

    using while and for loop in python
    - two simple codes that cover the working of loops with while and for.

---
+++ src/just-forTraining/for-loops.py
@@ -0,0 +1,4 @@
+friends = ["Rolf", "Jen", "Bob", "Anne"]
+
+for friend in range(4):
+    print(f"{friend} is my friend.")
---
+++ src/just-forTraining/while-loops.py
@@ -0,0 +1,14 @@
+number = 7
+
+while True:
+    user_input = input("Would you like to play? (Y/n)")
+
+    if user_input == "n":
+        break
+    user_number = int(input("Guess our number: "))
+    if user_number == number:
+        print("You guessed correctly!")
+    elif abs(number - user_number) == 1:
+        print("You were off by one.")
+    else:
+        print("Sorry, it's wrong!")
