bcaed33d4591bb71ca688e54328599a4a4774202
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-02 17:44:52 -0300

    fixing

--- src/just-forTraining/dictionaries_and_students_exercise.py
+++ src/just-forTraining/dictionaries_and_students_exercise.py
@@ -10,8 +10,10 @@ def average_grade_all_students(student_list):
     total = 0
     count = 0
     for student in student_list:
-        total = total + sum(student['grade'])
-        count = count + len(student['grade'])
+        total = + sum(student['grades'])
+        count = + len(student['grades'])
     return total / count
 
 
+average_grade(student)
+average_grade_all_students(student)
