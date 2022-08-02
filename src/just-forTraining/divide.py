b8691d2ac1412a059876a7be2b2ad6bb18b522d3
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-02 15:47:56 -0300

    new codes that were developed using function
    - learning the difference between function, argument and parameter;
    - divisor and dividend function.

---
+++ src/just-forTraining/divide.py
@@ -0,0 +1,8 @@
+def divide(dividend, divisor):
+    if divisor != 0:
+        print(dividend / divisor)
+    else:
+        print("You fool!")
+
+
+divide(15, 0)
---
+++ src/just-forTraining/function_arguments_parameters.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    result = x + y
+    print(result)
+
+
+add(5, 3)
+
+
+def say_hello(name):
+    print(f"Hello, {name}")
+
+
+say_hello("Jatto")
