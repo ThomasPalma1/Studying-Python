6b2917e82c238db4e47c041d276fdc8d9b510183
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-02 15:35:43 -0300

    learning and developing a function example in python
    - the example that was developed is based on a function that transforms the age entered into its version in seconds.

---
+++ src/just-forTraining/user_age_in_seconds.py
@@ -0,0 +1,7 @@
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {age_seconds}.")
+
+
+user_age_in_seconds()
