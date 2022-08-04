f46dbd822568cb75c3e1466cb352458ca301bcc4
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-04 16:18:07 -0300

    introduction to object oriented programming in python

---
+++ src/just-forTraining/object_oriented_programming.py
@@ -0,0 +1,12 @@
+class Student:
+    def __int__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student = Student("Bob", (90, 90, 93, 78, 90))
+print(student.name)
+print(student.average_grade())
