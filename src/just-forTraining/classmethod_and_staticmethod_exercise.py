ab392a8aa02b285f44b10a1368887a779344d635
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-09 15:58:34 -0300

    classmethod and staticmethod exercise

---
+++ src/just-forTraining/classmethod_and_staticmethod_exercise.py
@@ -0,0 +1,24 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        self.items.append({
+            'name': name,
+            'price': price
+        })
+
+    def stock_price(self):
+        total = 0
+        for item in self.items:
+            total += item['price']
+        return total
+
+    @classmethod
+    def franchise(cls, store):
+        return cls(store.name + " - Franchise")
+
+    @staticmethod
+    def store_details(store):
+        return '{}, total stock price: {}'.format(store.name, int(store.stock_price))
