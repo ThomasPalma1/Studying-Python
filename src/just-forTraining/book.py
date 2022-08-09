5e29264bdbc5d373ae66e22067e3313c2c17a9fb
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-09 15:46:37 -0300

    book factory example
    - classmethod was used;
    - in this example it was possible to implement __repr__.

---
+++ src/just-forTraining/book.py
@@ -0,0 +1,25 @@
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name, book_type, weight):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return cls(name, cls.TYPES[1], page_weight)
+
+
+book = Book.hardcover("Monster Kanzenban", 1500)
+light = Book.paperback("Vagabond", 600)
+
+print(book)
+print(light)
---
+++ src/just-forTraining/class_methods_and_static_methods.py
@@ -0,0 +1,14 @@
+class ClassTest:
+    def instance_method(self):
+        print(f"Called instance_method of {self}")
+
+    @classmethod
+    def class_method(cls):
+        print(f"Called class_method of {cls}")
+
+    @staticmethod
+    def static_method():
+        print("Called static_method.")
+
+
+ClassTest.class_method()
