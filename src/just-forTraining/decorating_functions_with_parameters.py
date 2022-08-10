3484624f2e6259395b114d04e880619679bf6d76
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-10 17:56:53 -0300

    decorators with parameters

--- src/just-forTraining/decorating_functions_with_parameters.py
+++ src/just-forTraining/decorating_functions_with_parameters.py
@@ -1,25 +1,36 @@
 import functools
 
-user = {"username": "osu", "access_level": "guest"}
+user = {"username": "xevel", "access_level": "guest"}
 
 
-def make_secure(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if user["access_level"] == "admin":
-            return func(*args, **kwargs)
-        else:
-            return f"No admin permissions for {user['username']}"
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}"
 
-    return secure_function
+        return secure_function
 
-
-@make_secure
-def get_password(panel):
-    if panel == "admin":
-        return "1234"
-    elif panel == "billing":
-        return "super_secure_password"
+    return decorator
 
 
-print(get_password("billing"))
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 12345"
+
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+print(get_admin_password())
+print(get_dashboard_password())
+
+user = {"username": "jatto", "access_level": "admin"}
+
+print(get_admin_password())
+print(get_dashboard_password())
